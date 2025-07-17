8778c81a5ca533015651cf33973265e22c928530
devozz32 <talkoragi25@gmail.com>
2025-07-17 18:19:51 +0300

    test-new

--- python_katas/kata_1/test.py
+++ python_katas/kata_1/test.py
@@ -1,228 +1,92 @@
 import unittest
-from python_katas.kata_1 import questions
-from python_katas.utils import unittest_runner
+from python_katas.kata_1.questions import *
 
 
-
-class TestSumOfElements(unittest.TestCase):
-    def test_empty_list(self):
-        lst = []
-        self.assertEqual(questions.sum_of_element(lst), 0)
-
-    def test_single_element(self):
-        lst = [5]
-        self.assertEqual(questions.sum_of_element(lst), 5)
-
-    def test_multiple_elements(self):
-        lst = [1, 2, 3]
-        self.assertEqual(questions.sum_of_element(lst), 6)
-
-
+class TestSumOfElement(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(sum_of_element([1, 2]), 3)
+        self.assertEqual(sum_of_element([]), 0)
 
 class TestVerbing(unittest.TestCase):
-    """
-   ## hello from nofar 1 Katas
-    """
+    def test_short_word(self):
+        self.assertEqual(verbing("go"), "go")
 
-    def test_string_longer_than_3_not_include_ing(self):
-        word = 'teach'
-        self.assertEqual(questions.verbing(word), 'teaching')
+    def test_add_ing(self):
+        self.assertEqual(verbing("walk"), "walking")
 
-
-
+    def test_add_ly(self):
+        self.assertEqual(verbing("swimming"), "swimmingly")
 
 class TestWordsConcatenation(unittest.TestCase):
-    """
-    1 Katas
-    """
-    def test_concatenates_words_with_spaces(self):
-        words = ['take', 'me', 'home']
-        expected = 'take me home'
-        self.assertEqual(questions.words_concatenation(words), expected)
-
+    def test_words(self):
+        self.assertEqual(words_concatenation(['take', 'me', 'home']), 'take me home')
+        self.assertEqual(words_concatenation([]), '')
 
 class TestReverseWordsConcatenation(unittest.TestCase):
-    """
-    1 Katas
-    """
-    class TestReverseWordsConcatenation(unittest.TestCase):
-     def test_basic(self):
-        expected = 'home me take'
-        self.assertEqual(questions.reverse_words_concatenation(['take', 'me', 'home']), expected)
-
-
+    def test_reverse(self):
+        self.assertEqual(reverse_words_concatenation(['take', 'me', 'home']), 'home me take')
 
 class TestIsUniqueString(unittest.TestCase):
-    """
-    2 Katas
-    """
-    class TestAllUniqueCharacters(unittest.TestCase):
-     def test_all_unique(self):
-        self.assertTrue(questions.is_unique_string("abcde"))
-
-
+    def test_unique(self):
+        self.assertTrue(is_unique_string("abcde"))
+        self.assertFalse(is_unique_string("aabc"))
+        self.assertTrue(is_unique_string(""))
 
 class TestListDiff(unittest.TestCase):
-    """
-    1 Katas
-    """
-    class TestDiffList(unittest.TestCase):
-     def test_regular_list(self):
-        expected = [None, 1, 1, 1, 3, 4]
-        self.assertEqual(questions.diff_list([1, 2, 3, 4, 7, 11]), expected)
-
-
+    def test_diff(self):
+        self.assertEqual(list_diff([1, 2, 3]), [None, 1, 1])
+        self.assertEqual(list_diff([]), [])
+        self.assertEqual(list_diff([5]), [None])
 
 class TestPrimeNumber(unittest.TestCase):
-    """
-    1 Katas
-    """
-    def test_is_prime(self):
-        num = 2
-        expected = True
-        self.assertEqual(questions.is_prime(num), expected)
-
-
+    def test_prime(self):
+        self.assertTrue(prime_number(7))
+        self.assertFalse(prime_number(4))
+        self.assertFalse(prime_number(1))
 
 class TestPalindromeNum(unittest.TestCase):
-
-     def test_TestPalindromeNum(self):
-        num = 424
-        expected = True
-        self.assertEqual(questions.pair_match(num), expected)
+    def test_palindrome(self):
+        self.assertTrue(palindrome_num(1221))
+        self.assertFalse(palindrome_num(123))
 
 class TestPairMatch(unittest.TestCase):
-    """
-    3 Katas
-    """
-    class TestClosestAgePair(unittest.TestCase):
-     def test_basic_match(self):
-        men = {"Avi": 30, "Ben": 25}
-        women = {"Dana": 24, "Rivka": 29}
-        expected = ("Ben", "Dana") 
-        self.assertEqual(questions.pair_match(men, women), expected)
+    def test_match(self):
+        men = {"John": 20, "Abraham": 45}
+        women = {"July": 18, "Kim": 26}
+        self.assertEqual(pair_match(men, women), ("John", "July"))
 
-    
-
-    
-class TestAverage(unittest.TestCase):
-    def test_average_integers(self):
-        expected = 4.0
-        self.assertEqual(questions.bad_average(2, 4, 6), expected)
+class TestBadAverage(unittest.TestCase):
+    def test_average(self):
+        self.assertAlmostEqual(bad_average(1, 2, 3), 2.0)
 
 class TestBestStudent(unittest.TestCase):
-    """
-    1 Katas
-    """
-
-class best_studenttop_student(unittest.TestCase):
-    def test_top_student(self):
-        grades = {"Ben": 78, "Hen": 88, "Natan": 99, "Efraim": 65, "Rachel": 95}
-        expected = "Natan"
-        self.assertEqual(questions.best_student(grades),expected)
-
-
-
-class TestPrintDictAsTable(unittest.TestCase):
-
-    class TestPrintDict(unittest.TestCase):
-     def test_print_dict(self):
-        grades = {
+    def test_best(self):
+        data = {
             "Ben": 78,
             "Hen": 88,
             "Natan": 99,
             "Efraim": 65,
             "Rachel": 95
         }
-
-        expected_output = (
-            "Key\tValue\n"
-            "-------------\n"
-            "Ben\t78\n"
-            "Hen\t88\n"
-            "Natan\t99\n"
-            "Efraim\t65\n"
-            "Rachel\t95\n"
-        )
-        print(grades)
-        self.assertEqual(questions.print_dict_as_table, expected_output)
-
-    """
-
-    ""
-
-
+        self.assertEqual(best_student(data), "Natan")
 
 class TestMergeDicts(unittest.TestCase):
-    """
-
-class TestMergeDicts(unittest.TestCase):
-    def test_merge_two_simple_dicts(self):
-        d1 = {'a': 1}
-        d2 = {'b': 2}
-        expected = {'a': 1, 'b': 2}
-        self.assertEqual(questions.merge_dicts(d1.copy(), d2.copy()), expected)
-
-    def test_merge_with_overlapping_keys(self):
-        d1 = {'a': 1, 'b': 2}
-        d2 = {'b': 99, 'c': 3}
-        expected = {'a': 1, 'b': 99, 'c': 3}
-        self.assertEqual(questions.merge_dicts(d1.copy(), d2.copy()), expected)
-
-    def test_merge_empty_dicts(self):
-        d1 = {}
-        d2 = {}
-        expected = {}
-        self.assertEqual(questions.merge_dicts(d1, d2), expected)
-
-    def test_merge_with_empty_dict2(self):
-        d1 = {'x': 5}
-        d2 = {}
-        expected = {'x': 5}
-        self.assertEqual(questions.merge_dicts(d1.copy(), d2), expected)
-    """
-
-
+    def test_merge(self):
+        self.assertEqual(merge_dicts({'a': 1}, {'b': 2}), {'a': 1, 'b': 2})
 
 class TestSevenBoom(unittest.TestCase):
-    """
-    class TestSevenBoom(unittest.TestCase):
-     def test_SevenBoom(self):
-        expected = [7, 14, 17, 21, 27, 28]
-        self.assertEqual(questions.seven_boom(30), expected)
-
-    def test_no_boom(self):
-        expected = []
-        self.assertEqual(questions.seven_boom(6), expected)
-
-    def test_includes_70(self):
-        self.assertIn(70, questions.seven_boom(75))
-
-    def test_last_is_77(self):
-        expected_last = 77
-        self.assertEqual(questions.seven_boom(77)[-1], expected_last)
-
-
+    def test_boom(self):
+        self.assertEqual(seven_boom(30), [7, 14, 17, 21, 27, 28])
 
 class TestCaesarCipher(unittest.TestCase):
-   def test_TestCaesarCipher(self):
-        stringx = "Fly Me To The Moon"
-        expected = "Iob Ph Wr Wkh Prrq"
-        self.assertEqual(questions.caesar_cipher(stringx), expected)
-
+    def test_cipher(self):
+        self.assertEqual(caesar_cipher("Fly Me To The Moon"), "Iob Ph Wr Wkh Prrq")
 
 class TestSumOfDigits(unittest.TestCase):
-    ""
-
-
-    def test_sum_of_digits(self):
-        nums =("51")
-        expected = 6
-        self.assertEqual(questions.sum_of_digits(nums), expected)
-
+    def test_sum(self):
+        self.assertEqual(sum_of_digits("2524"), 13)
+        self.assertEqual(sum_of_digits("00232"), 7)
+        self.assertEqual(sum_of_digits(""), 0)
 
 if __name__ == '__main__':
-    import inspect
-    import sys
-
-    unittest_runner(inspect.getmembers(sys.modules[__name__], inspect.isclass))
+    unittest.main()
